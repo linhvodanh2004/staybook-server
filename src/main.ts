3d35b8c824681d412979f8275c462eefad64e85b
linhvodanh2004 <linhvodanh2004@gmail.com>
2026-03-19 01:42:24 +0700

    corssss

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Kích hoạt CORS để cho phép Web (hoặc Web Admin) gọi được API
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
