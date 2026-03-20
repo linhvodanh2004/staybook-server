c3570e8908869689e87db311617b52bf57840329
linhvodanh2004 <linhvodanh2004@gmail.com>
2026-03-20 17:55:27 +0700

    ok

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -9,7 +9,10 @@ export class FirebaseService implements OnModuleInit {
     if (!admin.apps.length) {
       const projectId = process.env.FIREBASE_PROJECT_ID;
       const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-      const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+      let privateKey = process.env.FIREBASE_PRIVATE_KEY;
+      if (privateKey) {
+        privateKey = privateKey.replace(/^"|"$/g, '').replace(/\\n/g, '\n');
+      }
 
       if (projectId && clientEmail && privateKey) {
         this.logger.log('Initializing Firebase with explicit environment variables (Render/Prod)');
